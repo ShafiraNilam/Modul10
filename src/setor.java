3af12abfd639a28e084557095702f1ed0058f455
Shafira Nilamsari M <ns369shafira@gmail.com>
2017-02-11 21:06:20 +0700

    finish commit

---
+++ src/setor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+import javax.swing.JOptionPane;
+class setor {
+    private int saldo;
+    public void menabung (int bal, int s){
+        if(s%100!=0)
+            JOptionPane.showMessageDialog(null,
+            "Mesin ATM BRI tidak menerima uang koin \nSilahkan ulangi kembali",
+            "Transaksi Gagal", 0);
+        else if(s<50000)
+            JOptionPane.showMessageDialog(null,
+            "Besaran minimal setor tunai adalah Rp 50000,00", "ERRROR",0);
+        else if(s<50000){
+            saldo = bal+s;
+            JOptionPane.showMessageDialog(null, "Saldo Anda saat ini sebesar : "+ saldo);
+            
+        }
+    }
+    public int getsaldo(){
+        return saldo;
+    }
+}
